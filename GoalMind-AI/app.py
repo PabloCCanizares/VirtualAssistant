1f3456d41dd1ec065cc16e5e3eeeaea03dbca2e5
Chloe221201 <mengxiazhou@gmail.com>
2025-11-08 18:01:45 +0100

    Funcionalidad objetivos

--- GoalMind-AI/app.py
+++ GoalMind-AI/app.py
@@ -3,6 +3,7 @@ from flask import Flask
 from database.mongo_conn import init_app
 from controllers.task_controller import task_bp
 from controllers.dashboard_controller import dashboard_bp
+from controllers.goal_controller import goal_bp  
 
 app = Flask(__name__)
 app.secret_key = secrets.token_hex(32)
@@ -12,6 +13,7 @@ app.mongo_remote = remote
 
 app.register_blueprint(task_bp)
 app.register_blueprint(dashboard_bp)
+app.register_blueprint(goal_bp)  
 
 
 if __name__ == "__main__":
