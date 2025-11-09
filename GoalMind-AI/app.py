39a0f8a29fa4b9c903f410ac365538e6aea6f0cb
Chloe221201 <mengxiazhou@gmail.com>
2025-11-09 18:25:13 +0100

    calendario básico
    Falta por mejorar css, alarmas e integrar con objetivos y tareas

--- GoalMind-AI/app.py
+++ GoalMind-AI/app.py
@@ -4,6 +4,8 @@ from database.mongo_conn import init_app
 from controllers.task_controller import task_bp
 from controllers.dashboard_controller import dashboard_bp
 from controllers.goal_controller import goal_bp  
+from controllers.calendar_controller import calendar_bp
+
 
 app = Flask(__name__)
 app.secret_key = secrets.token_hex(32)
@@ -13,7 +15,8 @@ app.mongo_remote = remote
 
 app.register_blueprint(task_bp)
 app.register_blueprint(dashboard_bp)
-app.register_blueprint(goal_bp)  
+app.register_blueprint(goal_bp) 
+app.register_blueprint(calendar_bp) 
 
 
 if __name__ == "__main__":
